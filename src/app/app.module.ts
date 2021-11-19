753d2978063202e15fc29d0e2d894345f1c041a8
Victor Luiz <victorlhdev@gmail.com>
2021-11-19 16:05:15 -0300

    v1.1
    components criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { EstoqueComponent } from './components/estoque/estoque.component';
+import { NoticiasComponent } from './components/noticias/noticias.component';
+import { TrabalheConoscoComponent } from './components/trabalhe-conosco/trabalhe-conosco.component';
+import { ContatoComponent } from './components/contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SobreComponent,
+    EstoqueComponent,
+    NoticiasComponent,
+    TrabalheConoscoComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
