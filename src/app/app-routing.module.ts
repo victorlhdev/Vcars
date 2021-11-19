dab2ec62b8c73e9ae7aaacf9797e92ab18c053eb
Victor Luiz <victorlhdev@gmail.com>
2021-11-19 16:10:43 -0300

    v1.2
    rotas ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContatoComponent } from './components/contato/contato.component';
+import { EstoqueComponent } from './components/estoque/estoque.component';
+import { HomeComponent } from './components/home/home.component';
+import { NoticiasComponent } from './components/noticias/noticias.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { TrabalheConoscoComponent } from './components/trabalhe-conosco/trabalhe-conosco.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'sobre', component: SobreComponent},
+  { path: 'estoque', component: EstoqueComponent},
+  { path: 'noticias', component: NoticiasComponent},
+  { path: 'trabalhe_conosco', component: TrabalheConoscoComponent},
+  { path: 'contato', component: ContatoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
