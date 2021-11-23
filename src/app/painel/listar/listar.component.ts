47376e3d273bb3fc8a2601dca1729a140e025004
Victor Luiz <victorlhdev@gmail.com>
2021-11-23 00:35:03 -0300

    s1.2

--- src/app/painel/listar/listar.component.ts
+++ src/app/painel/listar/listar.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar',
   templateUrl: './listar.component.html',
-  styleUrls: ['./listar.component.scss']
+  styleUrls: ['./listar.component.scss'],
 })
 export class ListarComponent implements OnInit {
+  listaCarros: any[] = [
+    {
+      id: 1,
+      marca: 'Fiat',
+      modelo: 'Punto',
+      versao: 'Attractive',
+      ano: 2011,
+      km: 48.0,
+      preco: 35000,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
